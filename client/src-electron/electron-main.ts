75578b0b272c069b80ae778818f4956bf0459a59
sirantoinek <sirantoine.kevin@ufl.edu>
2025-09-25 15:52:45 -0400

    refactor: move handlers from electron-main and store.ts to src-electron/services

--- client/src-electron/electron-main.ts
+++ client/src-electron/electron-main.ts
@@ -1,13 +1,11 @@
-/* Updated on 9/20/2025 by Maria Pasaylo - 
-added handlers for getting and setting user name in electron-store
+/* Updated on 9/20/2025 by Maria Pasaylo -
+added handlers for getting and setting username in electron-store
 **/
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
 import os from 'os';
 import { fileURLToPath } from 'url'
-import { ipcMain } from 'electron';
-import { read, create, update, deleteEntry} from './sqlitedb';
-import { store } from './store';
+import { registerHandlers } from "./services/handlers";
 
 // needed in case process is undefined under Linux
 const platform = process.platform || os.platform();
@@ -32,7 +30,7 @@ async function createWindow() {
       preload: path.resolve(
         currentDir,
         path.join(process.env.QUASAR_ELECTRON_PRELOAD_FOLDER, 'electron-preload' + process.env.QUASAR_ELECTRON_PRELOAD_EXTENSION)
-),
+      ),
     },
   });
 
@@ -55,6 +53,11 @@ async function createWindow() {
   mainWindow.on('closed', () => {
     mainWindow = undefined;
   });
+
+  await app.whenReady().then(() => { // Based on: https://www.electronjs.org/docs/latest/tutorial/ipc
+    // Registers all ipcMain handlers for APIs exposed in electron-preload
+    registerHandlers();
+  })
 }
 
 void app.whenReady().then(createWindow);
@@ -69,32 +72,4 @@ app.on('activate', () => {
   if (mainWindow === undefined) {
     void createWindow();
   }
-});
-
-ipcMain.handle('sqliteRead', (event, key: string) => {
-  return read(key);
-});
-
-ipcMain.handle('sqliteCreate', (event, key: string, value: string) => {
-  create(key, value);
-  return { success: true };
-});
-
-ipcMain.handle('sqliteUpdate', (event, key: string, value: string) => {
-  update(key, value);
-  return { success: true };
-});
-
-ipcMain.handle('sqliteDelete', (event, key: string) => {
-  deleteEntry(key);
-  return { success: true };
-});
-
-ipcMain.handle('getStoreName', () => {
-  return store.get('name');
-});
-
-ipcMain.handle('setStoreName', (event, name: string) => {
-  store.set('name', name);
-  return true;
 });
---
+++ client/src-electron/services/handlers.ts
@@ -0,0 +1,36 @@
+import {ipcMain} from "electron";
+import {create, deleteEntry, read, update} from "app/src-electron/sqlitedb";
+import {store} from "app/src-electron/services/store";
+
+export function registerHandlers()
+{
+  // SQLite Handlers
+  ipcMain.handle('sqliteRead', (event, key: string) => {
+    return read(key);
+  });
+
+  ipcMain.handle('sqliteCreate', (event, key: string, value: string) => {
+    create(key, value);
+    return true;
+  });
+
+  ipcMain.handle('sqliteUpdate', (event, key: string, value: string) => {
+    update(key, value);
+    return true;
+  });
+
+  ipcMain.handle('sqliteDelete', (event, key: string) => {
+    deleteEntry(key);
+    return true;
+  });
+
+  // electron-store Handlers
+  ipcMain.handle('getStoreName', () => {
+    return store.get('name');
+  });
+
+  ipcMain.handle('setStoreName', (event, name: string) => {
+    store.set('name', name);
+    return true;
+  });
+}
