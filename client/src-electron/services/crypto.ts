7d1be9c80a3309027adc94341128ae2013cbd4db
Michael Jagiello <michaeljr623@hotmail.com>
2025-09-27 20:34:56 -0400

    feat!: add pad32 helper and hashing wrapper functions, rename encrypt/decrypt functions, lessen input restrictions on encrypt/decrypt

--- client/src-electron/services/crypto.ts
+++ client/src-electron/services/crypto.ts
@@ -3,7 +3,8 @@
  * Created: 2025-09-27
  * Updated: 2025-09-27
  *
- * This file defines functions for AES256 encryption.
+ * This file defines wrapper functions for AES256 encryption/decryption, as well as a padding helper function that sets a string to 32 characters.
+ * It also contains wrapper functions for SHA hashing algorithms 256, 512/256, and 512.
  *
  * This file is a part of OpenOrganizer.
  * This file and all source code within it are governed by the copyright and license terms outlined in the LICENSE file located in the top-level directory of this distribution.
@@ -12,16 +13,37 @@
 
 import crypto from 'crypto';
 
-export function encrypt32(data: string, key: string) {
-  if (data.length != 32 || key.length != 32) return null;
-  const cipher = crypto.createCipheriv('aes-256-gcm', key, key);
-  const encr = Buffer.from(cipher.update(data));
-  return encr;
+// pads a string and limits it to 32 characters
+export function pad32(str: string): string {
+  const nullString = '\0'.repeat(32);
+  return str.concat(nullString).substring(0, 32);
 }
 
-export function decrypt32(data: Buffer, key: string) {
-  if (data.length != 32 || key.length != 32) return null;
-  const decipher = crypto.createDecipheriv('aes-256-gcm', key, key)
-  const decr = Buffer.from(decipher.update(data));
-  return decr;
+// encrypt data string using AES256
+// key must be 32 bytes long
+export function encrypt(data: Buffer | string, key: Buffer | string, iv: Buffer | string) {
+  const cipher = crypto.createCipheriv('aes-256-gcm', key, iv);
+  return Buffer.from(cipher.update(data));
+}
+
+// decrypt data string using AES256
+// key must be 32 bytes long
+export function decrypt(data: Buffer, key: Buffer | string, iv: Buffer | string) {
+  const decipher = crypto.createDecipheriv('aes-256-gcm', key, iv)
+  return Buffer.from(decipher.update(data));
+}
+
+// hash using sha256, return 32-byte data buffer
+export function hash256(data: Buffer | string) {
+  return crypto.createHash('sha256').update(data).digest();
+}
+
+// hash using sha512_256, return 32-byte data buffer
+export function hash512_256(data: Buffer | string) {
+  return crypto.createHash('sha512-256').update(data).digest();
+}
+
+// hash using sha512, return 64-byte data buffer
+export function hash512(data: Buffer | string) {
+  return crypto.createHash('sha512').update(data).digest();
 }
