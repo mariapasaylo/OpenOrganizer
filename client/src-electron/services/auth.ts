323a094be9fec50b17d8b5442d561f16cd3d96d1
Maria Pasaylo <90634830+mariapasaylo@users.noreply.github.com>
2025-10-28 02:38:50 -0400

    feat: user can log in

--- client/src-electron/services/auth.ts
+++ client/src-electron/services/auth.ts
@@ -12,7 +12,7 @@
  * No part of OpenOrganizer, including this file, may be reproduced, modified, distributed, or otherwise used except in accordance with the terms specified in the LICENSE file.
  */
 
-import {hash256, hash512_256, generatePrivateKey, encrypt} from "app/src-electron/services/crypto";
+import {hash256, hash512_256, generatePrivateKey, encrypt, decrypt} from "app/src-electron/services/crypto";
 import Store from 'electron-store';
 import type {Schema} from 'electron-store';
 
@@ -81,12 +81,20 @@ function setAuthToken(authToken : Buffer) {
   accountStore.set('authToken', authToken);
 }
 
-function getPrivateKey() {
-  return Buffer.from(accountStore.get('privateKey'));
+function getPrivateKey1() {
+  return Buffer.from(accountStore.get('privateKey1'));
 }
 
-function setPrivateKey(privateKey : Buffer) {
-  accountStore.set('privateKey', privateKey);
+function setPrivateKey1(privateKey : Buffer) {
+  accountStore.set('privateKey1', privateKey);
+}
+
+function getPrivateKey2() {
+  return Buffer.from(accountStore.get('privateKey2'));
+}
+
+function setPrivateKey2(privateKey : Buffer) {
+  accountStore.set('privateKey2', privateKey);
 }
 
 function getUserId() {
@@ -101,10 +109,10 @@ export async function createAccount(username : string, password : string): Promi
   // hash password, generate and store privateKey, encrypt privateKey with SHA256(password)
   setUsername(username);
   setPassword(password);
-  setPrivateKey(generatePrivateKey());
+  setPrivateKey1(generatePrivateKey());
   const hashKeyPassword: Buffer = hash256(password);
   const hashServerPassword: Buffer = hash512_256(password);
-  const encryptedPrivateKey: Buffer = encrypt(getPrivateKey(), hashKeyPassword, hashKeyPassword);
+  const encryptedPrivateKey: Buffer = encrypt(getPrivateKey1(), hashKeyPassword, hashKeyPassword);
   
   //Note do not send 0 for username
   const userData = Buffer.alloc(128,20); 
@@ -133,10 +141,11 @@ export async function createAccount(username : string, password : string): Promi
       body: userData
     });
 
-    // Parse the reponse
+    //Parse the reponse
     const responseData = Buffer.from(await response.arrayBuffer());
     
-    console.log('Response data', responseData.length); //why is it length 43 instead of 40?
+    //More testing
+    console.log('Response data', responseData.length);
     console.log('Response data', responseData.toString('utf8'));
     console.log(response.status);
 
@@ -144,12 +153,12 @@ export async function createAccount(username : string, password : string): Promi
     const userIdBytes = responseData.slice(0, 8);
     const authTokenBytes = responseData.slice(8, 40);
     setAuthToken(authTokenBytes);
-    //read as little endian and need to convert to string because electron-store does not support bigint
+    //read as little endian and need to convert to string because electron-store json does not support bigint
     setUserId(userIdBytes.readBigInt64LE(0).toString());
     
     
   } catch (error) {
-    console.error("Error registering account:", error);
+    console.error("Error registering account: ", error);
     return false;
   }
 
@@ -158,8 +167,10 @@ export async function createAccount(username : string, password : string): Promi
 
 
   export async function loginAccount(username : string, password : string): Promise<boolean> {
-    //hash the password
+    //hash the passwords
     const hashServerPassword: Buffer = hash512_256(password);
+    const hashKeyPassword: Buffer = hash256(password);
+
     //Ensure username is max 32 bytes
     const usernameBuffer = Buffer.from(username).slice(0,32);
 
@@ -172,23 +183,41 @@ export async function createAccount(username : string, password : string): Promi
     console.log('LOG IN USER DATA', userData.toString('utf8'));
     console.log('LOG IN USER DATA RAW', userData);
     console.log('LOG IN USER DATA LENGTH', userData.length);
-    //MARIA START HERE AFTER YOU TEST THE ABOVE
-    // Sending in raw data via API request to /login
-    // try {
-    //   const response = await fetch ("http://localhost:3001/login",{
-    //     method: 'POST',
-    //     headers: {'Content-Type':''}
-    //   });
-    // } catch (error){
-    //   return false;
-    // }
 
-    //parse and store the userID and authToken and encrypted private keys
+    //Sending in raw data via API request to /login
+    try {
+      const response = await fetch ("http://localhost:3001/login",{
+        method: 'POST',
+        headers: {'Content-Type': 'application/octet-stream'},
+        body: userData
+      });
 
-    //if valid login and password
-    //store userID and authToken
-    //decrypt private keys and store them
-    //else display message
+
+      // Parse and store the userID, authToken, decrypt encrypted private keys
+      const responseData = Buffer.from(await response.arrayBuffer());
+
+      //More testing
+      console.log('Response data', responseData.length);
+      console.log('Response data', responseData.toString('utf8'));
+      console.log(response.status);
+
+      //userID [0:8], authToken[8:40], privateKey1[40:72], privateKey2[72:104]
+      const userIdBytes = responseData.slice(0, 8);
+      const authTokenBytes = responseData.slice(8, 40);
+      const encrPrivateKey1 = responseData.slice(40,72);
+      const encrPrivateKey2 = responseData.slice(72,104);
+
+      setAuthToken(authTokenBytes);
+      //read as little endian and need to convert to string because electron-store json does not support bigint
+      setUserId(userIdBytes.readBigInt64LE(0).toString());
+      // Decrypt private keys and store them
+      setPrivateKey1(decrypt(encrPrivateKey1, hashKeyPassword, hashKeyPassword));
+      setPrivateKey2(decrypt(encrPrivateKey2, hashKeyPassword, hashKeyPassword));
+
+    } catch (error){
+      console.error("Error logging into account: ", error);
+      return false;
+    }
 
 
     return true;
