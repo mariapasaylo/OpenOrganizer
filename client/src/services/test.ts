7fc5118c0787efa96c2152d1c7dc48930b4b82d0
Michael Jagiello <michaeljr623@hotmail.com>
2025-11-04 20:25:07 -0500

    feat: add init() to App.vue and set up to be able to optionally run testing suite upon launch

--- client/src/services/test.ts
+++ client/src/services/test.ts
@@ -1,20 +1,20 @@
-<!--
+/*
  * Authors: Michael Jagiello
- * Created: 2025-04-13
+ * Created: 2025-11-04
  * Updated: 2025-11-04
  *
- * This file handles the overarching router view and launches the initialization function.
+ * This file defines the entry testing suite function and the function used to test the Renderer.
  *
  * This file is a part of OpenOrganizer.
  * This file and all source code within it are governed by the copyright and license terms outlined in the LICENSE file located in the top-level directory of this distribution.
  * No part of OpenOrganizer, including this file, may be reproduced, modified, distributed, or otherwise used except in accordance with the terms specified in the LICENSE file.
--->
+ */
 
-<template>
-  <router-view />
-</template>
+export function TestingSuite() {
+    TestingSuiteRenderer();
+    //TestingSuiteElectron();
+}
 
-<script setup lang="ts">
-import { init } from 'src/services/init';
-await init();
-</script>
+function TestingSuiteRenderer() {
+    console.log("renderer testing");
+}
